1b504e5f0ac65b30dcbaaf6597e539ab6496d780
Eric Douglas <eric.douglas.mail@gmail.com>
2017-08-27 19:32:28 -0300

    First commit

---
+++ lib/staircase.ex
@@ -0,0 +1,18 @@
+defmodule Staircase do
+  @moduledoc """
+  Documentation for Staircase.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Staircase.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/staircase_test.exs
@@ -0,0 +1,8 @@
+defmodule StaircaseTest do
+  use ExUnit.Case
+  doctest Staircase
+
+  test "greets the world" do
+    assert Staircase.hello() == :world
+  end
+end
