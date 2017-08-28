29e60f010bc300a9afc1d42048bc53f8424fc0b6
Eric Douglas <eric.douglas.mail@gmail.com>
2017-08-27 21:14:26 -0300

    Add documentation and doctests

--- lib/staircase.ex
+++ lib/staircase.ex
@@ -1,16 +1,52 @@
 defmodule Staircase do
+  @moduledoc """
+  Generate staircases based on the input value
+  """
+
+  @doc """
+  Receives the input and print the staircase to the console
+  """
   def main(size) do
     Enum.each(1..size, fn i ->
       IO.puts(space_gen(size - i) <> symbol_gen(i))
     end)
   end
 
+  @doc """
+  Generate a string with spaces to complete the row
+
+  ## Example
+
+      iex> Staircase.space_gen(0)
+      ""
+
+      iex> Staircase.space_gen(1)
+      " "
+
+      iex> Staircase.space_gen(7)
+      "       "
+  """
   def space_gen(0), do: ""
   def space_gen(num) when num > 0 do
     Enum.reduce(1..num, "", fn(_i, acc) -> acc <> " " end)
   end
 
-  def symbol_gen(num) do
+  @doc """
+  Generate a string with `n` characters (`#`)
+
+  ## Example
+
+      iex> Staircase.symbol_gen(0)
+      ""
+
+      iex> Staircase.symbol_gen(1)
+      "#"
+
+      iex> Staircase.symbol_gen(7)
+      "#######"
+  """
+  def symbol_gen(0), do: ""
+  def symbol_gen(num) when num > 0 do
     Enum.reduce(1..num, "", fn(_i, acc) -> acc <> "#" end)
   end
 end
